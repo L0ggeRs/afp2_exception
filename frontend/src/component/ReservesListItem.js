8324ab89ac65e9794f8642c7bd79e2f96a30b04f
L0ggeRs <ukeve12@gmail.com>
2021-04-13 16:06:41 +0200

    Delete ReservesListItem.js

--- frontend/src/component/ReservesListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ReservesListItem({ReserveId, GuestId, CampId, CampingStyle, Electricity, Price, ReserveStart, ReserveEnd}){
-    return(
-        <span>({ReserveId}, {GuestId}, {CampId}, {CampingStyle}, {Electricity}, {Price}, {ReserveStart}, {ReserveEnd})</span>
-    );
-}
-
-export default ReservesListItem;
