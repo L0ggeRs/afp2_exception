53eb63c0e91ab0dc111365ba783faf07250342f7
Guti Adrián <62698226+GuthyX@users.noreply.github.com>
2021-04-13 10:19:33 +0200

    #12 Vendég listázása
    Componentben a GuestListitem létrehozása, ez listázza ki a Vendégeket

---
+++ frontend/src/component/GuestListitem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GuestListitem({GuestName,GuestID,GuestEmail,GuestPhone}){
+    return (
+        <span>({GuestName}, {GuestID}), {GuestEmail}, {GuestPhone}</span>
+    );
+}
+
+export default GuestListitem;
