95fa3e878f6b9bf9e05086b52d2bbd9d407aa53f
Guti Adrián <62698226+GuthyX@users.noreply.github.com>
2021-04-19 09:52:22 +0200

    #12 Zárójel hiba
    Rossz helyen volt egy zárójel

--- frontend/src/component/GuestListitem.js
+++ frontend/src/component/GuestListitem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function GuestListitem({GuestName,GuestID,GuestEmail,GuestPhone}){
     return (
-        <span>({GuestName}, {GuestID}), {GuestEmail}, {GuestPhone}</span>
+        <span>({GuestName}, {GuestID}, {GuestEmail}, {GuestPhone})</span>
     );
 }
 
