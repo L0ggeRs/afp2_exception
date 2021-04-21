e476dca7b09a62c83938bf6b8eba13808044c7e0
Karaba Márk <karaba.matyika@gmail.com>
2021-04-21 21:33:22 +0200

    Navbar vendég link létrehozása
    Kapcsolat oldal törlése és a vendég oldal létrehozása helyette

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./component/Navbar/Navbar";
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import GuestAdd from './component/Pages/GuestAddForm';
-import Contacts from './component/Pages/Contacts';
+import Reserve from './component/Pages/Reserve';
 import Homepage from './component/Pages/HomePage';
 import Pictures from './component/Pages/Pictures';
 import ReservedDates from './component/Pages/ReservedDates';
@@ -16,10 +16,10 @@ function App() {
         <Navbar />
             <Switch>
                 <Route path='/kezdolap'  component={Homepage} />
-                <Route path='/foglalas' exact component={GuestAdd} />
+                <Route path='/foglalas' exact component={Reserve} />
                 <Route path='/idopontok'  component={ReservedDates} />
                 <Route path='/kepek'  component={Pictures} />
-                <Route path='/elerhetosegek'  component={Contacts} />
+                <Route path='/vendeg'  component={GuestAdd} />
             </Switch>
             </Router>
         </div>
