713cca34a0899a54a082cc5a7996d30733644cb8
L0ggeRs <ukeve12@gmail.com>
2021-04-24 10:35:21 +0200

    #13 ReservationStore.js
    ReservationStore.js hozzáadása a store mappához.

---
+++ frontend/src/store/ReservationStore.js
@@ -0,0 +1,29 @@
+import {EventEmitter} from 'events';
+import dispatcher from "../dispatcher/Dispatcher";
+import {refresh} from '../dispatcher/ReservationActionConstants';
+
+class ReservationsStore extends EventEmitter{
+
+    reservations = [];
+
+    emitChange(){
+        this.emit('Change');
+    }
+
+    addChangeListener(callback){
+        this.addListener('Change',callback);
+    }
+
+    removeChangeListener(callback){
+        this.removeListener('Change',callback);
+    }
+}
+
+const store = new ReservationsStore();
+export default store;
+
+dispatcher.register(({action,payload})=>{
+    if(action !== refresh ) return;
+    store.reservations = payload;
+    store.emitChange();
+});
