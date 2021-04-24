4e295fdde3000b74c64ceb1b17ec70a23433e1e9
L0ggeRs <ukeve12@gmail.com>
2021-04-24 10:33:11 +0200

    #13 ReservationActionConstants.js
    ReservationActionConstants.js hozzáadása a dispatcher mappához.

---
+++ frontend/src/dispatcher/ReservationActionConstants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    refresh : 'REFRESH_RESERVATIONS',
+    showError : 'SHOW_RESERVATIONS_ERROR_MSG',
+    clearError : 'CLEAR_RESERVATIONS_ERROR_MSG',
+    deleteError: 'DELETE_ERROR'
+};
