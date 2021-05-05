44bd5acfa230efaccb1b9b0979b3fe99dbda1928
Adrian <g.a.a612.ag@gmail.com>
2021-05-05 13:09:55 +0200

    Features: ID- hiba uzenet.
    hiba uzenetet dob ha idnak nem számot adunk meg.

--- frontend/src/component/Pages/GuestAddForm.js
+++ frontend/src/component/Pages/GuestAddForm.js
@@ -8,13 +8,22 @@ class GuestAddForm extends React.Component {
         super(props);
         this.state = {
             GuestName: 'Béla',
-            GuestID: '0',
+            GuestID: '',
             GuestEmail: 'example@example.com',
             GuestPhone: '123456789',
         };
         this.formOnChange = this.formOnChange.bind(this);
     }
-
+        isNumber(n){
+            const re = /^[0-9\b]+$/;
+            if (n === '' || re.test(n)) {
+                let st = this.state;
+                st.GuestID = n;
+                this.setState({value: st })
+                return -1
+            }
+            return -2
+        }
     formOnChange(event) {
         const {name, value} = event.target;
         this.setState({[name]: value});
@@ -36,7 +45,7 @@ class GuestAddForm extends React.Component {
                                        onChange={(e) => {
                                            let st = this.state;
                                            st.GuestName = e.target.value;
-                                           this.setState(st);
+                                           this.state.setState(st);
                                        }}
                                 />
                             </div>
@@ -46,10 +55,12 @@ class GuestAddForm extends React.Component {
                                        type={"text"}
                                        value={this.state.GuestID}
                                        onChange={(e) => {
-                                           let st = this.state;
-                                           st.GuestID = e.target.value;
-                                           this.setState(st);
-                                       }}
+                                                  if (this.isNumber(e.target.value) == -2){
+                                                      alert('Hiba az ID megadásánál.\n Csak számot lehet megadni.',)
+                                                  }
+                                                }
+                                       }
+
                                 />
                             </div>
                             <div className="p-2"><h4>Email:</h4>
@@ -73,9 +84,11 @@ class GuestAddForm extends React.Component {
                                 <input className={"form-control myinput"} type={"string"}
                                        value={this.state.GuestPhone}
                                        onChange={(e) => {
-                                           let st = this.state;
-                                           st.GuestPhone = e.target.value;
-                                           this.setState(st);
+                                           if(this.isNumber(e.target.value)){
+                                               let st = this.state;
+                                               st.GuestPhone = e.target.value;
+                                               this.setState(st);
+                                           }
                                        }}
                                 />
                             </div>
