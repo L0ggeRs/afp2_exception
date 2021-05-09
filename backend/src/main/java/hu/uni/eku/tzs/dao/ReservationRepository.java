c51a3a580f7e2f3c04753137e6d639024b2d7c92
Bencsik Krisztián <bencsikkrisztian1999@gmail.com>
2021-05-09 17:11:21 +0200

    Reservation repository

--- backend/src/main/java/hu/uni/eku/tzs/dao/ReservationRepository.java
+++ backend/src/main/java/hu/uni/eku/tzs/dao/ReservationRepository.java
@@ -1,12 +1,11 @@
 package hu.uni.eku.tzs.dao;
 
 
-import hu.uni.eku.tzs.dao.entity.Guest;
 import hu.uni.eku.tzs.dao.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
 
-    Guest getGuestById(Integer guestId);
+    Reservation getReservationById(Integer reservationId);
 
 }
